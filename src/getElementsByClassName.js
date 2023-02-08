c48c4caf7f7a0377a39df6d665a85c68296ad249
Yale Yang <yalerkm25@gmail.com>
2023-02-07 23:11:09 -0800

    fix some small style edits

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -8,7 +8,7 @@
 // O: A list of element tags sharing that class name, pulled from the html of the window
 // C: None come to mind
 // E: class not present in html? Maybe some other junk input, check back for weird edges later.
-var getElementsByClassName = function(className, current_html
+var getElementsByClassName = function(className, currentHtml
 ) {
   // your code here
   // return array declare
@@ -17,29 +17,29 @@ var getElementsByClassName = function(className, current_html
   // assign current html if html is undefined
   // console.log('current html')
   // console.log(current_html)
-  if (current_html === undefined){
-    current_html = document; // the html of the current document, I think
+  if (currentHtml === undefined) {
+    currentHtml = document; // the html of the current document, I think
   }
 
   // if that element has  the class className, add it to return arra
   // console.log(current_html)
-  if (current_html.classList && current_html.classList.contains(className)){
-    returnArray.push(current_html);
+  if (currentHtml.classList && currentHtml.classList.contains(className)) {
+    returnArray.push(currentHtml);
   }
 
   // for each html element that is a child of the window
-  for (var node in current_html.children){
+  for (var node in currentHtml.children) {
     // skip functions for cleanliness
     // variable to make stuff look cleaner
-    var thisNode = current_html.children[node]
-    if (typeof(thisNode) !== 'object'){
+    var thisNode = currentHtml.children[node];
+    if (typeof(thisNode) !== 'object') {
       continue;
     }
 
     // call this function for the child, and put it in a variable
     var childElementArray = getElementsByClassName(className, thisNode);
     // push the kids
-    for (var childElement in childElementArray){
+    for (var childElement in childElementArray) {
       returnArray.push(childElementArray[childElement]);
     }
   }
